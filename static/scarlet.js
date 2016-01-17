6e68576bace2d08c5c2f4ef41fad727865a17204
Joshua Coats <joshu@fearchar.net>
2016-01-17 11:47:57 -0800

    Removing unnecessary dependencies; updating the JavaScript with some useful comments

--- static/scarlet.js
+++ static/scarlet.js
@@ -1,4 +1,11 @@
+// The page length (i.e., the number of results to scroll past before trying to load a new page)
+//   is set here to its default value of 2. If you’d like to change this, here’s one of the places
+//   to do so!
 const PAGE_LENGTH = 2;
+
+// `PAGE_OFFSET` determines how many pages we’ve already passed — by default, this is 0, since
+//   we tend to start at the beginning. However, if we’ve loaded a page that matches `from/N`,
+//   we should use a PAGE_OFFSET of `N`.
 const PAGE_OFFSET = (function () {
   const pageMatches = window.location.toString().match(/from\/(\d+)/);
   if (!pageMatches) {
@@ -7,6 +14,7 @@ const PAGE_OFFSET = (function () {
     return parseInt(pageMatches[1]);
   }})();
 
+// `scrolledPast(element)` generates an event whenever `element` is scrolled past in the window.
 function scrolledPast(element) {
   return Bacon.fromBinder(function (sink) {
     $(window).scroll(function (e) {
@@ -17,14 +25,20 @@ function scrolledPast(element) {
   });
 }
 
+// `toResultStream` will change our AJAX requests into a result stream for Bacon so that we can
+//    more easily interact with them in this framework.
 function toResultStream(request) {
   return Bacon.fromPromise($.ajax(request));
 }
 
+// `lastStubScrolled` keeps track of whether the final `div.post` element has been scrolled past,
+//    and, if it has been, whether there are any `stub` elements remaining after the `div.post`
 var lastStubScrolled = scrolledPast("div.post:last")
   .filter(function (el) {
     return (el.next()[0] === undefined || el.nextAll(".stub")[0] === undefined); });
 
+// `getMoreStubs` watches whether more stubs need to be populated on the page (i.e., whether the
+//    last stub has been scrolled past) and loads more if need be.
 var getMoreStubs = lastStubScrolled
   .filter(function (el) { return (el.nextAll("#thatsallfolks")[0] === undefined); })
   .map(function () { return Math.floor($("div.post").length / PAGE_LENGTH); })
@@ -33,13 +47,14 @@ var getMoreStubs = lastStubScrolled
     return "/next-after/" + (PAGE_OFFSET + Math.floor($("div.post").length / PAGE_LENGTH)); })
   .flatMap(toResultStream)
   .zip(lastStubScrolled);
-
 getMoreStubs.onError(function (err) {
   console.log(err);
 });
 
 getMoreStubs
   .onValues(function (newStubs, lastPost) {
+    //   should we receive an empty set of `newStubs`, we should add a marker to denote that there
+    //   are no more stubs to be loaded, if one doesn’t exist; we’ll call this `#thatsallfolks`.
     if (newStubs == "" && $("#thatsallfolks")[0] === undefined) {
       lastPost.after("<div id=\"thatsallfolks\" hidden=\"\"></div>");
     } else {
@@ -52,10 +67,14 @@ getMoreStubs
     }
   });
 
+// If we’ve scrolled past the last `div.post` and there _are_ still stubs, we should populate
+//    them into real posts.
+
+// First, `nextStubToPopulate` finds that stub, if it exists;
 var nextStubToPopulate = scrolledPast("div.post:last")
   .filter(function (el) { return el.nextAll(".stub")[0] != undefined })
   .map(function (el) { return $(el.nextAll(".stub")[0]); });
-
+// and then we fade the content in.
 nextStubToPopulate
   .map(function (el) { return el[0].id.replace(/^t/, "/just/"); })
   .flatMap(toResultStream)
